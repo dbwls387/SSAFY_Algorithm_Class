baf243dcb53429e98bb7932303a8baf934fe297c
SSAFY <SSAFY@DESKTOP-KVCQHCD>
2022-08-03 17:36:23 +0900

    알고리즘 3일차 과제 제출

--- src/Class/D_0803/BOJ_11660__Pro__2.java
+++ src/Class/D_0803/BOJ_11660__Pro__2.java
@@ -26,12 +26,12 @@ public class BOJ_11660__Pro__2 {
 			}
 		}
 
-		for (int i = 1; i < N + 1; i++) {
-			for (int j = 1; j < N + 1; j++) {
-				System.out.print(accu[i][j] + " ");
-			}
-			System.out.println();
-		}
+//		for (int i = 1; i < N + 1; i++) {
+//			for (int j = 1; j < N + 1; j++) {
+//				System.out.print(accu[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
 
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < M; i++) {
