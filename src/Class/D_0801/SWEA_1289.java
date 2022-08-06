e4bbe27830994f2a6c34f76b572e8256aca75040
dbwls387@naver.com <dbwls387@naver.com>
2022-08-07 02:05:01 +0900

    알고리즘 1일차

--- src/Class/D_0801/SWEA_1289.java
+++ src/Class/D_0801/SWEA_1289.java
@@ -1,56 +1,39 @@
-// SWEA 1289번 원재의 메모리 복구하기  
+// SWEA 1289번 원재의 메모리 복구하기
 
 package Class.D_0801;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class SWEA_1289 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static int T, cnt;
+	static char[] memory, input;
 
-		/*
-		 * int T = sc.nextInt(); for (int t = 1; t <= T; t++) { String input =
-		 * sc.next(); int memory = 0; int[] original = new int[input.length()];
-		 * 
-		 * for (int i = 0; i < original.length; i++) { original[i] = input.charAt(i) -
-		 * '0'; } int cnt = original[0];
-		 * 
-		 * for (int i = 0; i < original.length - 1; i++) { if (original[i] != original[i
-		 * + 1]) { cnt++; } }
-		 * 
-		 * System.out.println("#" + t + " " + cnt); }
-		 */
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
 
-		int T = sc.nextInt();
-		sc.nextLine(); // 개행문자 해소
+		for (int t = 1; t <= T; t++) {
+			input = br.readLine().toCharArray();
 
-		for (int t = 0; t < T; t++) {
-			int count = 0; // 변경 건수
-			String str = sc.nextLine(); // 개행문자까지
-			// 두개의 배열
-			// 1. inputArray / 2. memArray
-			int cnt = str.length();
-			int[] inputArray = new int[cnt];
-			int[] memArray = new int[cnt]; // 0으로 초기화
-
-			for (int i = 0; i < cnt; i++) {
-				inputArray[i] = str.charAt(i) - 48; // -'0'
+			memory = new char[input.length];
+			for (int i = 0; i < input.length; i++) {
+				memory[i] = '0';
 			}
 
-			// 두 배열을 같은 index로 이동하면서 값을 비교
-			for (int i = 0; i < cnt; i++) {
-				if (memArray[i] != inputArray[i]) {
-					// memArray를 뒤까지 변경
-					for (int j = i; j < cnt; j++) {
-						memArray[j] = inputArray[i];
+			cnt = 0;
+			for (int i = 0; i < input.length; i++) {
+				if (input[i] != memory[i]) {
+					for (int j = i; j < input.length; j++) {
+						memory[j] = input[i];
 					}
-					count++; // 변경된 수 증가
+					cnt++;
 				}
 			}
 
-			System.out.println("#" + t + " " + count);
+			System.out.println("#" + t + " " + cnt);
 		}
-
 	}
 
 }
