c068ef80b0bd2490128d333f4275b42d38de9b07
SSAFY <SSAFY@DESKTOP-KVCQHCD>
2022-08-10 17:12:32 +0900

    알고리즘 8일차 과제

---
+++ src/HW/D_0810/BOJ_1010.java
@@ -0,0 +1,27 @@
+// BOJ 1010번 다리 놓기 
+
+package HW.D_0810;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1010 {
+
+	static int T, N, M;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			
+			
+		}
+	}
+
+}
