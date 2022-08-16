284fdb00b39b7d907c90d6b86f26569e0537c109
dbwls387@naver.com <dbwls387@naver.com>
2022-08-16 23:41:20 +0900

    BOJ 1074번 Z

--- src/Class/D_0816/BOJ_1074.java
+++ src/Class/D_0816/BOJ_1074.java
@@ -2,11 +2,42 @@
 
 package Class.D_0816;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_1074 {
 
-	public static void main(String[] args) {
-		
+	static int N, r, c;
 
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+
+		int size, visit = 0;
+		while (N > 0) {
+			size = (int) Math.pow(2, (--N));
+
+			if (r < size && c < size) {
+				visit += 0;
+			} else if (r < size && c >= size) {
+				visit += size * size;
+				c -= size;
+			} else if (r >= size && c < size) {
+				visit += size * size * 2;
+				r -= size;
+			} else {
+				visit += size * size * 3;
+				r -= size;
+				c -= size;
+			}
+		}
+
+		System.out.println(visit);
 	}
 
 }
