ff44ce2bb7af1120efec4cb95aaa0166d389695d
SSAFY <SSAFY@DESKTOP-KVCQHCD>
2022-08-17 17:09:01 +0900

    BOJ 1074번 Z - 교수님 풀이

---
+++ src/Class/D_0817/BOJ_1074__Pro.java
@@ -0,0 +1,52 @@
+// BOJ 1074번 Z 
+
+package Class.D_0817;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1074__Pro {
+
+	static int N, r, c, ans;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		// N은 2의 제곱수이므로 N을 실제 좌, 우에 해당하는 길이로 보정
+
+		N = (int) Math.pow(2, N); // N은 2로 나눌 수 있다.
+
+		// 시작 원점
+		int y = 0;
+		int x = 0;
+
+		// 분할정복
+		while (N > 1) {
+			// 가로, 세로를 반으로 나누어서 4공간으로 분할
+			N /= 2;
+
+			// r, c가 4공간 중 어느 곳에 해당하는지 판단, 각 공간에서 r, c 값에 따라 ans 값 갱신, 원점 보정
+			if (r < y + N && c < x + N) { // top-left
+				;
+			} else if (r < y + N && c >= x + N) { // top-right
+				ans += N * N * 1;
+				x += N;
+			} else if (r >= y + N && c < x + N) { // bottom-left
+				ans += N * N * 2;
+				y += N;
+			} else { // bottom-right
+				ans += N * N * 3;
+				x += N;
+				y += N;
+			}
+		}
+
+		System.out.println(ans);
+	}
+
+}
