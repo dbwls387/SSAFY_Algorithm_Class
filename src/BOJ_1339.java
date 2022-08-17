49d37c66a6d49e7b840fd0040614721e328c54b3
SSAFY <SSAFY@DESKTOP-KVCQHCD>
2022-08-17 17:08:34 +0900

    스터디 BOJ 1339번 단어 수학 완료

--- src/BOJ_1339.java
+++ src/BOJ_1339.java
@@ -6,44 +6,34 @@ import java.util.Arrays;
 
 public class BOJ_1339 {
 
-	static int N;
-	static int[] num = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+	static int N, num, result;
 	static int[] alphabet = new int[26];
-	static String[] word;
+	static String word;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		word = new String[N];
+
 		for (int i = 0; i < N; i++) {
-			word[i] = br.readLine();
-		}
-
-		/*
-		int leng = 0;
-		for (int i = 0; i < word.length; i++) {
-			char[] ch = word[i].toCharArray();
-			leng += ch.length;
-		}
-		char[] ch = new char[leng];
-
-		int num_idx = 0;
-		int idx = 0;
-		int n = 0;
-		for (int i = 0; i < leng; i++) {
-			char[] str = word[n++].toCharArray();
-			if (alphabet[str[idx] - 65] == 0) {
-				alphabet[str[idx++] - 65] = num[num_idx++];
+			word = br.readLine();
+			for (int j = 0; j < word.length(); j++) {
+				alphabet[word.charAt(j) - 'A'] += (Math.pow(10, word.length() - j - 1));
 			}
-			if (n == N - 1) {
-				n = 0;
-			}
-		}
-		*/
 
-		
-		
-		System.out.println(Arrays.toString(alphabet));
+		}
+
+		Arrays.sort(alphabet);
+
+		num = 9;
+		result = 0;
+		for (int i = 25; i >= 0; i--) {
+			if (alphabet[i] == 0)
+				break;
+			result += alphabet[i] * num;
+			num--;
+		}
+
+		System.out.println(result);
 	}
 
 }
