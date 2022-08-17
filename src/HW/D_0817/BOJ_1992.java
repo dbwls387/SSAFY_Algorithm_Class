02963ef1d06d3b2b47719b10e22f683fe4115f30
dbwls387@naver.com <dbwls387@naver.com>
2022-08-17 21:44:31 +0900

    [과제] BOJ 1992번 쿼드트리 (8/17)

--- src/HW/D_0817/BOJ_1992.java
+++ src/HW/D_0817/BOJ_1992.java
@@ -4,13 +4,13 @@ package HW.D_0817;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class BOJ_1992 {
 
 	static int N;
 	static String result = "";
 	static int[][] arr;
+	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,23 +24,36 @@ public class BOJ_1992 {
 			}
 		}
 
-		quad(0, 0);
+		quad(0, 0, N);
+
+		System.out.println(sb.toString());
 	}
 
-	static void quad(int y, int x) {
-		if (N == 1)
+	static void quad(int x, int y, int N) {
+		int num = arr[x][y];
+
+		if (N == 1) {
+			sb.append(num);
 			return;
+		}
 
-		System.out.print("(");
-		N /= 2;
-
-		for (int i = y; i < y + N; i++) {
-			for (int j = x; j < x + N; j++) {
-				if (arr[i][j] == 1) {
-					
+		for (int i = x; i < x + N; i++) {
+			for (int j = y; j < y + N; j++) {
+				if (arr[i][j] != num) {
+					N /= 2;
+					sb.append("(");
+					quad(x, y, N);
+					quad(x, y + N, N);
+					quad(x + N, y, N);
+					quad(x + N, y + N, N);
+					sb.append(")");
+					return;
 				}
 			}
 		}
+
+		sb.append(num);
+		return;
 	}
 
 }
