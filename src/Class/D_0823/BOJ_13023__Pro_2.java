19ba93041071f6c2665e37ce07e1a4ca9532f652
SSAFY <SSAFY@DESKTOP-KVCQHCD>
2022-08-23 17:22:17 +0900

    BOJ 13023번 ABCDE

---
+++ src/Class/D_0823/BOJ_13023__Pro_2.java
@@ -0,0 +1,82 @@
+// BOJ 13023번 ABCDE 
+// 인접 리스트로 
+
+package Class.D_0823;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_13023__Pro_2 {
+
+	static int N, M;
+//	static boolean[][] friends; // matrix
+	static List<List<Integer>> friends = new ArrayList<>();
+
+	// visit -> bit masking 으로 대체
+	static boolean done;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+//		friends = new boolean[N][N];
+
+		for (int i = 0; i < N; i++) {
+			friends.add(new ArrayList<>());
+		}
+
+		// 입력 처리
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+//			friends[a][b] = friends[b][a] = true;
+			friends.get(a).add(b);
+			friends.get(b).add(a);
+		}
+
+		// dfs 탐색
+		// 모든 정점에서 따져본다. 가능한 경우를 만나면 바로 종료
+		for (int i = 0; i < N; i++) {
+			dfs(i, 0, 1 << i);
+		}
+
+		if (done)
+			System.out.println(1);
+		else
+			System.out.println(0);
+	}
+
+	static void dfs(int num, int cnt, int visit) {
+		// 기저조건
+		if (done)
+			return;
+
+		if (cnt == 4) {
+			done = true;
+			return;
+		}
+
+		friends.get(num).forEach((i) -> {
+			if ((visit & (1 << i)) == 0) {
+				dfs(i, cnt + 1, visit | (1 << i));
+			}
+		});
+
+		// 모든 정점을 대상으로
+//		for (int i = 0; i < N; i++) {
+//			if (i == num || (visit & (1 << i)) != 0)
+//				continue;
+//
+//			if (friends[num][i])
+//				dfs(i, cnt + 1, visit | (1 << i));
+//		}
+	}
+}
