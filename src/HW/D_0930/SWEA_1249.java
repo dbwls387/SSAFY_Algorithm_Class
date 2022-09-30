3f260ca0141286db0f375dfb5496718014ac33cd
dbwls387@naver.com <dbwls387@naver.com>
2022-09-30 17:31:38 +0900

    0930 과제 푸는 중!!

---
+++ src/HW/D_0930/SWEA_1249.java
@@ -0,0 +1,12 @@
+// SWEA 1249번 [S/W 문제해결 응용] 4일차 - 보급로 
+
+package HW.D_0930;
+
+public class SWEA_1249 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
