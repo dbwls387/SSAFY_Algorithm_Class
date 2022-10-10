e5804a40837c523fac07571f6b1aa324e9b0295c
dbwls387@naver.com <dbwls387@naver.com>
2022-10-10 18:11:19 +0900

    낚시왕

--- src/Class/D_1005/BOJ_17143.java
+++ src/Class/D_1005/BOJ_17143.java
@@ -6,11 +6,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.StringTokenizer;
 
 public class BOJ_17143 {
 
 	static int R, C, M, ans;
+	static Node[][] map;
 
 	static List<Node> shark = new ArrayList<>();
 
@@ -26,137 +28,131 @@ public class BOJ_17143 {
 		C = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
+		map = new Node[R + 1][C + 1];
+
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-
 			int r = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			int s = Integer.parseInt(st.nextToken());
 			int d = Integer.parseInt(st.nextToken());
 			int z = Integer.parseInt(st.nextToken());
 
-			shark.add(new Node(r, c, s, d, z, i));
+			shark.add(new Node(r, c, s, d, z));
+			map[r][c] = new Node(r, c, s, d, z);
 		}
 
 		for (int i = 1; i <= C; i++) {
-			fishing(i);
+			fishing(i); // 낚시왕이 있는 열에서 땅과 제일 가까운 상어 잡기
+			bfs(); // 상어 이동
+			samePlace(); // 같은 칸에 있는 상어 잡아먹기
 		}
 
 		System.out.println(ans);
 	}
 
-	static void fishing(int px) { // px 는 사람이 서있는 열
+	static void fishing(int px) { // px는 낚시왕이 서있는 열
+		for (int i = 1; i <= R; i++) {
+			if (map[i][px] != null) {
+				ans += map[i][px].z;
+				shark.remove(map[i][px]);
+				map[i][px] = null;
 
-		System.out.println("----------------------");
-		for (int i = 0; i < shark.size(); i++) {
-			Node n = shark.get(i);
-			System.out.println(n.y + ", " + n.x + " / i: " + n.i + " / 방향: " + n.d + " / 속력: " + n.s);
-		}
-
-		List<Node> list = new ArrayList<>();
-
-		for (int i = 0; i < shark.size(); i++) {
-			Node node = shark.get(i);
-
-			if (node.x == px) {
-				list.add(node);
+				return;
 			}
 		}
-
-		if (list.size() > 0) {
-			int kill = list.get(0).y;
-			int killIdx = 0;
-			for (int i = 1; i < list.size(); i++) {
-				if (kill > list.get(i).y) {
-					kill = list.get(i).y;
-					killIdx = i;
-				}
-			}
-			Node sn = list.get(killIdx);
-			ans += sn.z; // 잡은 상어 크기
-			shark.remove(sn); // 가장 가까운 상어 잡기
-		}
-
-		bfs();
 	}
 
-	static void bfs() { // 상어 움직이기
-		for (int i = shark.size() - 1; i >= 0; i--) {
+	static void bfs() { // 상어 이동
+		int size = shark.size();
+
+		for (int i = 0; i < size; i++) {
 			Node node = shark.get(i);
 
-			int d = node.d;
 			int ny = node.y;
 			int nx = node.x;
 
-			for (int j = 0; j < node.s; j++) {
+			int d = node.d; // 방향
+			int s = node.s; // 속력
+
+			for (int j = 0; j < s; j++) {
+				if (ny == 1 && d == 1)
+					d = 2;
+				else if (ny == R && d == 2)
+					d = 1;
+				else if (nx == 1 && d == 4)
+					d = 3;
+				else if (nx == C && d == 3)
+					d = 4;
+
 				ny += dy[d];
 				nx += dx[d];
-
-				if (ny <= 1 || nx <= 1 || ny >= R || nx >= C) {
-					if (d == 1)
-						d = 2;
-					else if (d == 2)
-						d = 1;
-					else if (d == 3)
-						d = 4;
-					else if (d == 4)
-						d = 3;
-
-					continue;
-				}
 			}
 
 			node.y = ny;
 			node.x = nx;
 			node.d = d;
-			
-//			shark.remove(i);
-//			shark.add(new Node(ny, nx, node.s, d, node.z, node.i));
-		}
-
-		// 같은 칸에 상어가 두마리 이상 있을 경우 ( 큰 상어가 나머지 상어를 잡아 먹음 )
-		int size = shark.size();
-		List<Node> samePlace = new ArrayList<>();
-
-		int idx = 0;
-		while (idx > size) {
-			for (int j = idx + 1; j < size; j++) {
-				Node A = shark.get(idx);
-				Node B = shark.get(j);
-
-				if (A.y == B.y && A.x == B.x) { // 상어가 같은 칸에 있을 때
-					samePlace.add(A);
-					samePlace.add(B);
-				}
-			}
-
-			if (samePlace.size() > 0) {
-				int max = samePlace.get(0).s;
-
-				for (int j = 1; j < samePlace.size(); j++) {
-					if (samePlace.get(j).s > max) {
-						max = samePlace.get(j).s;
-						shark.remove(samePlace.get(j - 1));
-						size--;
-					}
-				}
-			}
-
-			idx++;
 		}
 
 	}
 
-	static class Node {
-		int y, x, s, d, z, i; // s: 속력, d: 이동 방향, z: 크기, i: shark 리스트에서의 idx
+	static void samePlace() {
+		// map 초기화
+		for (int i = 1; i <= R; i++) {
+			for (int j = 1; j <= C; j++) {
+				map[i][j] = null;
+			}
+		}
 
-		public Node(int y, int x, int s, int d, int z, int i) {
+		// map에 현재 상어 정보 집어넣기
+		int size = shark.size();
+		for (int i = size - 1; i >= 0; i--) {
+			Node node = shark.get(i);
+
+			// 겹치는 상어가 존재할 경우
+			if (map[node.y][node.x] != null) {
+				int curShark = map[node.y][node.x].z;
+				int nextShark = node.z;
+
+				// 현재 맵에 있는 상어보다 크면 얘로 넣어주기
+				if (nextShark > curShark) {
+					shark.remove(map[node.y][node.x]);
+					map[node.y][node.x] = node;
+				} else {
+					shark.remove(node);
+				}
+			} else {
+				map[node.y][node.x] = node;
+			}
+		}
+	}
+
+	static class Node {
+		int y, x, s, d, z; // (r, c): 상어 위치 , s: 속력, d: 이동 방향, z: 크기
+
+		public Node(int y, int x, int s, int d, int z) {
 			this.y = y;
 			this.x = x;
 			this.s = s;
 			this.d = d;
 			this.z = z;
-			this.i = i;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (o instanceof Node) {
+				Node n = (Node) o;
+				if (this.y == n.y && this.x == n.x && this.s == n.s && this.d == n.d && this.z == n.z) {
+					return true;
+				}
+				return false;
+			}
+			return false;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(y, x, s, d, z);
 		}
 	}
 
